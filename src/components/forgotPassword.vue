6e479388a308891d61c6c7437d6fdd539ff9759c
Gerti Goga <gertigoga@gmail.com>
2020-07-30 18:02:15 -0700

    Added non-functional forgot password page.

--- src/components/forgotPassword.vue
+++ src/components/forgotPassword.vue
@@ -1,13 +1,110 @@
 <template>
-  <div>forgot password</div>
+  <v-container class="centerAlign">
+    <v-card class="mx-auto text-center" max-width="344">
+      <v-card-text class="cardTextStyle">
+        <div class="text--primary logoFont">Canvas</div>
+      </v-card-text>
+      <v-card-actions>
+        <v-form ref="form" v-model="valid">
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  class="formFont"
+                  v-model="email"
+                  :rules="emailRules"
+                  label="E-mail"
+                  required
+                  ><template slot="append"
+                    ><span
+                      ><font-awesome-icon
+                        :icon="['far', 'envelope']"
+                        class="mr-2 mb-1 envelopeStyle"
+                      /> </span></template
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-btn
+                  block
+                  color="success"
+                  class="formFont"
+                  :disabled="!valid"
+                  @submit.prevent
+                  @click="logInEmail"
+                  ><font-awesome-icon
+                    :icon="['fa', 'sign-in-alt']"
+                    class="mr-2 mb-1"
+                  />
+                  Send Link</v-btn
+                >
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-form>
+      </v-card-actions>
+      <section class="mt-5">
+        <router-link to="/" class="linkStyle"
+          ><v-btn block color="grey darken-1" class="formFont backBtnStyle"
+            >Back To Login</v-btn
+          ></router-link
+        >
+      </section>
+    </v-card>
+    <v-card class="formFont mx-auto text-center mt-5" max-width="344">
+      <v-card-text class="cardTextStyle">
+        <div>Need an account?</div>
+        <router-link to="/signup" class="linkStyle">Sign Up</router-link>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-export default {
+import { mapActions } from 'vuex'
+import { mapFields } from 'vuex-map-fields'
 
+export default {
+  data: function () {
+    return {
+      valid: false,
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+      ]
+    }
+  },
+  computed: {
+    ...mapFields(['email'])
+  },
+  methods: {
+    ...mapActions(['logInEmail'])
+  }
 }
 </script>
 
-<style>
-
+<style scopped>
+.centerAlign {
+  margin-top: 10vh !important;
+}
+.logoFont {
+  font-size: 2.125rem;
+  font-weight: 400;
+  line-height: 2.5rem;
+  letter-spacing: 0.0073529412em;
+  font-family: "Fredericka the Great", cursive;
+}
+.formFont {
+  font-family: "Josefin Slab", serif;
+  font-weight: bold;
+}
+.cardTextStyle {
+  padding: 8px;
+}
+.linkStyle {
+  text-decoration: none;
+  font-weight: bold;
+}
+.backBtnStyle {
+  color: white !important;
+}
 </style>
